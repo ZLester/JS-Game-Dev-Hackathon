894062d2135116460ae1652e97335960e43bbcfc
Zachary Lester <zachary.greg.lester@gmail.com>
2016-01-30 14:17:37 -0800

    Update game.js

--- client/js/game.js
+++ client/js/game.js
@@ -1,12 +1,15 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
 
 var mainState = {
+  
   preload: function () {
     game.stage.backgroundColor = '#666';
     game.load.image('player', 'assets/hrlogo.png'); 
     game.load.image('ground', 'assets/ground.png');
   },
+  
   create: function () {
+    
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     this.player = this.game.add.sprite(100, 245, 'player');
@@ -18,35 +21,37 @@ var mainState = {
 
     this.platforms = game.add.group();
     this.platforms.enableBody = true;
+    
     this.ground = [];
     for (var i = 0; i < game.world.width; i+=70) {
       this.ground.push(this.platforms.create(i, game.world.height - 70, 'ground'));
     }
+    
     this.platforms.forEach(function(platform) {
       platform.body.immovable = true;
     });
     
   },
+  
   update: function () {
+    
     for (var i = 0; i < this.ground.length; i++) {
       game.physics.arcade.collide(this.player, this.ground[i]);
     }
+    
     this.player.body.velocity.x = 0;
 
     var cursors = game.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown)
-    {
+    if (cursors.left.isDown) {
         this.player.body.velocity.x = -300;
-    }
-    else if (cursors.right.isDown)
-    {
+    } else if (cursors.right.isDown) {
         this.player.body.velocity.x = 300;
     }
-    if (cursors.up.isDown && this.player.body.touching.down)
-    {
+    if (cursors.up.isDown && this.player.body.touching.down) {
         this.player.body.velocity.y = -800;
     }
   }
+  
 };
 
 game.state.add('main', mainState);
